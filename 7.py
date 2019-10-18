61f60412a1fcfe48e0d7e91584778d53d8bd821a
Oliver <ocben1@gmail.com>
2019-10-19 10:27:58 +1100

    challenge

---
+++ 7.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+driver= webdriver.Chrome()
+
+driver.get("http://www.seleniumhq.org")
+
+element_id = driver.find_element_by_id('q')
+print(element_id)
+
+element_name = driver.find_element_by_name('q')
+print(element_name)
+
+heading_xpath = driver.find_element_by_xpath("//*[@id='mainContent']/h2[1]")
+print(heading_xpath)
+
+element_classname = driver.find_element_by_class_name('selenium-sponsors')
+print(element_classname)
+
+driver.close()
