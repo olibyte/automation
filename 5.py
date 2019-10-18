70259f21d17acc6b66264a86bd6be6593b17947c
Oliver <ocben1@gmail.com>
2019-10-19 09:22:02 +1100

    locate elements using XPath

---
+++ 5.py
@@ -0,0 +1,12 @@
+#locate elements using XPath
+from selenium import webdriver
+driver= webdriver.Chrome()
+driver.get("file:///C:/Users/ocben/pythonAutomation/page.html")
+login_form_absolute = driver.find_element_by_xpath("/html/body/form[1]")
+login_form_relative = driver.find_element_by_xpath("//form[1]")
+login_form_id = driver.find_element_by_xpath("//form[@id='loginForm']")
+print("My login form is:")
+print(login_form_absolute)  
+print(login_form_relative)
+print(login_form_id)
+driver.close()
