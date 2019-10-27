3d6ae0c97333409b3d1347019c9a27ba0eefd242
Oliver <ocben1@gmail.com>
2019-10-27 11:54:06 +1100

    parsing and serializing JSON

---
+++ requests/xml_json_web/json_serialize_start.py
@@ -0,0 +1,29 @@
+# Process JSON data returned from a server
+
+# use the JSON module
+import json
+
+
+def main():
+    # define a python ditcionary
+    pythonData = {
+        "sandwich": "Reuben",
+        "toasted": True,
+        "toppings": ["Thousand Island Dressing",
+                     "Sauerkraut",
+                     "Pickles"
+                     ],
+        "price": 8.99
+    }
+
+    # TODO: serialize to JSON using dumps
+    
+    # jsonStr = json.dumps(pythonData)
+    jsonStr = json.dumps(pythonData, indent=4)
+
+    # TODO: print the resulting JSON string
+    print("JSON Data: --------")
+    print(jsonStr)
+
+if __name__ == "__main__":
+    main()
