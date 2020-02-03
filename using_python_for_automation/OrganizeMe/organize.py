07ebf599be6caaafbd5bcc002b3d6d02ca4d5f3e
Oliver <ocben1@gmail.com>
2020-02-04 10:04:37 +1100

    organizing directories

--- using_python_for_automation/OrganizeMe/organize.py
+++ using_python_for_automation/OrganizeMe/organize.py
@@ -1,6 +1,24 @@
+import os
+from pathlib import Path
 SUBDIRECTORIES = {
     "DOCUMENTS": ['.pdf','.rtf','.txt'],
     "AUDIO": ['.m4a','.m4b','.mp3'],
     "VIDEOS": ['.mov','.avi','.mp4'],
     "IMAGES": ['.jpg','.jpeg','.png']
 }
+def pickDirectory(value):
+    for category, suffixes in SUBDIRECTORIES.items():
+        for suffix in suffixes:
+            if suffix == value:
+                return category
+print(pickDirectory('.pdf'))
+
+def organizeDirectory():
+    for items in os.scandir():
+        filePath = Path(item)
+        fileType = filePath.suffix.lower()
+        directory = pickDirectory(fileType)
+        directoryPath = Path(directory)
+        if directoryPath.is_dir() != True:
+            directoryPath.mkdir()
+        filePath.rename(directoryPath.joinpath(filePath))
---
+++ using_python_for_automation/OrganizeMe/script.py
@@ -0,0 +1,4 @@
+import subprocess
+
+for i in range(0,5):
+    subprocess.check_call(['py', 'using_python_for_automation\OrganizeMe\example.py'])
