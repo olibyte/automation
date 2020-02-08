d015492e266b9dd6542e7ed9b8f3533ccbee148c
Oliver <ocben1@gmail.com>
2020-02-08 15:24:57 +1100

    selenium explicit wait functions

---
+++ using_python_for_automation/selenium/dragAndDrop.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+driver = webdriver.Firefox()
+driver.maximize_window()
+driver.get('http://dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html')
+source = driver.find_element_by_xpath('//*[@id="box3"]')
+dest = driver.find_element_by_xpath('//*[@id="box103"]')
+actions = ActionChains(driver)
+actions.drag_and_drop(source, dest).perform()
